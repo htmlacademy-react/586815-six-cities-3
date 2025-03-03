84b9cf148592345ed14de3091207d211c9786475
Даниил Топ <cool.topolskiy.gmail.com>
2025-03-03 14:56:02 +0300

    Configure sorting

--- src/components/main-offers/offers-section.tsx
+++ src/components/main-offers/offers-section.tsx
@@ -4,6 +4,8 @@ import { useState } from 'react';
 import { Nullable } from 'vitest';
 import Map from '../map/map';
 import { classNamesMap } from '../../const';
+import Sorting from './sorting';
+import { getSortedOffers } from '../../utils/sort';
 
 type Props = {
   offers: OfferType[];
@@ -14,12 +16,15 @@ type Props = {
 function OffersSection(props: Props): JSX.Element {
   const { offers, currentCity, currentCityLocation } = props;
   const [activeOffer, setActiveOffer] = useState<Nullable<OfferType>>(null);
+  const [sortedOffers, setSortedOffers] = useState<OfferType[]>(offers);
 
   const handleOfferHover = (offer: Nullable<OfferType>) => {
     setActiveOffer(offer || null);
   };
 
-  const cardList = offers.map((offer: OfferType) => (
+  const handleSortingOptionChange = (sortType: string) => setSortedOffers(getSortedOffers(offers, sortType));
+
+  const cardList = sortedOffers.map((offer: OfferType) => (
     <OfferCard
       key={offer.id}
       cardData={offer}
@@ -33,21 +38,7 @@ function OffersSection(props: Props): JSX.Element {
       <section className="cities__places places">
         <h2 className="visually-hidden">Places</h2>
         <b className="places__found">{offers.length} places to stay in {currentCity}</b>
-        <form className="places__sorting" action="#" method="get">
-          <span className="places__sorting-caption">Sort by</span>
-          <span className="places__sorting-type" tabIndex={0}>
-            Popular
-            <svg className="places__sorting-arrow" width="7" height="4">
-              <use xlinkHref="#icon-arrow-select"></use>
-            </svg>
-          </span>
-          <ul className="places__options places__options--custom places__options--opened">
-            <li className="places__option places__option--active" tabIndex={0}>Popular</li>
-            <li className="places__option" tabIndex={0}>Price: low to high</li>
-            <li className="places__option" tabIndex={0}>Price: high to low</li>
-            <li className="places__option" tabIndex={0}>Top rated first</li>
-          </ul>
-        </form>
+        <Sorting onSortOptionChange={handleSortingOptionChange} />
         <div className="cities__places-list places__list tabs__content">
           {cardList}
         </div>
--- src/const.ts
+++ src/const.ts
@@ -46,3 +46,10 @@ export const currentCustomIcon = leaflet.icon({
   iconSize: [27, 39],
   iconAnchor: [13.5, 39],
 });
+
+export const SortingOptions = {
+  POPULAR: 'Popular',
+  PRICE_LOW_TO_HIGH: 'Price: low to high',
+  PRICE_HIGH_TO_LOW: 'Price: high to low',
+  TOP_RATED_FIRST: 'Top rated first',
+};
