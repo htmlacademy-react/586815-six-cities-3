f1e989e4508cff4f66aa1d84ffca2ccfc0fc64e3
Даниил Топ <cool.topolskiy.gmail.com>
2025-03-04 11:27:45 +0300

    Refactor sort listener

--- src/components/main-offers/sorting.tsx
+++ src/components/main-offers/sorting.tsx
@@ -6,34 +6,27 @@ type Props = {
   onSortOptionChange: (sortType: string) => void;
 };
 
-const enum ClassNamesSortingList {
-  SORTING_OPTIONS_CLOSED = 'places__options--closed',
-  SORTING_OPTIONS_OPENED = 'places__options--opened',
-}
-
 function Sorting(props: Props): JSX.Element {
   const { onSortOptionChange: onSortingOptionChange } = props;
-  const [classNameSortingList, setClassNameSortingList] = useState(ClassNamesSortingList.SORTING_OPTIONS_CLOSED);
+  const [isOpen, setIsOpen] = useState(false);
   const [currentOption, setCurrentOption] = useState(SortingOptions.POPULAR);
   const sortingRef = useRef<HTMLFormElement>(null);
   const sortingOptions = Object.values(SortingOptions);
 
   const handleDropdownToggle = () => {
-    setClassNameSortingList((prevState) => prevState === ClassNamesSortingList.SORTING_OPTIONS_CLOSED
-      ? ClassNamesSortingList.SORTING_OPTIONS_OPENED
-      : ClassNamesSortingList.SORTING_OPTIONS_CLOSED);
+    setIsOpen(!isOpen);
   };
 
   const handleOutsideDropdownClick = (evt: MouseEvent) => {
     if (sortingRef.current && !sortingRef.current.contains(evt.target as Node)) {
-      setClassNameSortingList(ClassNamesSortingList.SORTING_OPTIONS_CLOSED);
+      setIsOpen(false);
     }
   };
 
   const handleOptionChange = (option: string) => {
     setCurrentOption(option);
     onSortingOptionChange(option);
-    setClassNameSortingList(ClassNamesSortingList.SORTING_OPTIONS_CLOSED);
+    setIsOpen(false);
   };
 
   useEffect(() => {
@@ -62,7 +55,8 @@ function Sorting(props: Props): JSX.Element {
         </svg>
       </span>
       < ul
-        className={classNames('places__options places__options--custom', classNameSortingList)}
+        className={classNames('places__options places__options--custom',
+          isOpen ? 'places__options--opened' : 'places__options--closed')}
       >
         {sortingOptions.map((option) => (
           <li
