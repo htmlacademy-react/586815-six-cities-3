b09f29b6ecfd5f41bbad9f763976024abe40954b
Даниил Топ <cool.topolskiy.gmail.com>
2025-03-10 15:17:19 +0300

    Configure NoFavoritesList

--- src/pages/favorites.tsx
+++ src/pages/favorites.tsx
@@ -4,6 +4,7 @@ import { Helmet } from 'react-helmet-async';
 import { OfferType } from '../types/common';
 import FavoritesCity from '../components/favorites/favorites-city';
 import { Link } from 'react-router-dom';
+import NoFavoritesList from '../components/no-favorites-list';
 
 type Props = {
   favoritesOffers: OfferType[];
@@ -36,17 +37,18 @@ export default function Factories(props: Props): JSX.Element {
           </div>
         </div>
       </header>
-
-      <main className="page__main page__main--favorites">
-        <div className="page__favorites-container container">
-          <section className="favorites">
-            <h1 className="favorites__title">Saved listing</h1>
-            <ul className="favorites__list">
-              {cityList}
-            </ul>
-          </section>
-        </div>
-      </main>
+      {favoritesOffers.length ?
+        <main className="page__main page__main--favorites">
+          <div className="page__favorites-container container">
+            <section className="favorites">
+              <h1 className="favorites__title">Saved listing</h1>
+              <ul className="favorites__list">
+                {cityList}
+              </ul>
+            </section>
+          </div>
+        </main> :
+        <NoFavoritesList />}
       <footer className="footer container">
         <Link className="footer__logo-link" to="/">
           <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33" />
