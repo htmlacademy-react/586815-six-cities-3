1d5c527bd5852d31490be46acd5d4fd3a1b46e2e
Даниил Топ <cool.topolskiy.gmail.com>
2025-03-31 12:52:57 +0300

    Configure tests v8

--- src/components/private-route.tsx
+++ src/components/private-route.tsx
@@ -6,28 +6,28 @@ import { useAppSelector } from '../hooks/store';
 import { AuthStatus } from '../const';
 
 type PrivateRouteProps = {
-	children: JSX.Element;
-	reverse?: boolean;
+  children: JSX.Element;
+  reverse?: boolean;
 }
 
 function PrivateRoute(props: PrivateRouteProps): JSX.Element {
-	const { children, reverse } = props;
-	const isAuth = useAuth();
-	const authStatus = useAppSelector((state) => state.user.authStatus);
+  const { children, reverse } = props;
+  const isAuth = useAuth();
+  const authStatus = useAppSelector((state) => state.user.authStatus);
 
-	if (authStatus === AuthStatus.Unknown) {
-		return <Loader />;
-	}
+  if (authStatus === AuthStatus.Unknown) {
+    return <Loader />;
+  }
 
-	if (reverse && isAuth) {
-		return <Navigate to={AppRoute.Main} replace />;
-	}
+  if (reverse && isAuth) {
+    return <Navigate to={AppRoute.Main} replace />;
+  }
 
-	if (!reverse && !isAuth) {
-		return <Navigate to={AppRoute.Login} replace />;
-	}
+  if (!reverse && !isAuth) {
+    return <Navigate to={AppRoute.Login} replace />;
+  }
 
-	return children;
+  return children;
 }
 
 export default PrivateRoute;
